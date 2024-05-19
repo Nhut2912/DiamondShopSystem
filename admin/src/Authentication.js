a66d61c0fa2d250827d494a1ec87757c2cf1cddf
Nhut2912 <nhuttmse172865@fpt.edu.vn>
2024-05-20 01:34:14 +0700

    admin pages form

---
+++ admin/src/Authentication.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './theme/Authentication.css'
+function Authentication() {
+  return (
+    <div className='container'>
+        <div className='login-form'>
+            <div className='welcome'>
+
+            </div>
+            <div className='login-information'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Authentication;
