6f9d57a53437158f6c62622215b146793a5799d3
Nhut2912 <nhuttmse172865@fpt.edu.vn>
2024-05-20 17:02:02 +0700

    back end archi

--- admin/src/Authentication.js
+++ admin/src/Authentication.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './theme/Authentication.css'
+
 function Authentication() {
   return (
     <div className='container'>
         <div className='login-form'>
             <div className='welcome'>
-
+                
             </div>
             <div className='login-information'>
-
+                  
             </div>
         </div>
     </div>
