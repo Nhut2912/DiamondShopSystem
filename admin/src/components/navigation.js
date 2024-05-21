8eeedd61ad1f24a0c05856469ce4b38ea0f615b9
Nhut2912 <nhuttmse172865@fpt.edu.vn>
2024-05-21 18:25:23 +0700

    Navigation done

--- admin/src/components/navigation.js
+++ admin/src/components/navigation.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import '../theme/Navigation.css';
 import { ICONS } from "../constants";
 
 const Navigation = () => {
+
+    const [activeItem, setActiveItem] = useState('Dashboard'); // Initial active item
+
+    const handleClick = (item) => {
+      setActiveItem(item);
+    };
+   
+    const navigationItems = [
+        { name: 'Dashboard', icon: ICONS.icon_dashboard, icon_active : ICONS.icon_dashboard_active },
+        { name: 'Product', icon: ICONS.icon_product , icon_active : ICONS.icon_product_active},
+        { name: 'Order', icon: ICONS.icon_orders , icon_active : ICONS.icon_order_active},
+        { name: 'Account', icon: ICONS.icon_user , icon_active : ICONS.icon_user_active},
+        { name: 'Warranty', icon: ICONS.icon_warranty , icon_active : ICONS.icon_warranty_active},
+        { name: 'Promotions', icon: ICONS.icon_promotions , icon_active : ICONS.icon_product_active},
+        { name: 'Gold Price', icon: ICONS.icon_gold_price , icon_active : ICONS.icon_gold_price_active},
+        { name: 'Diamond Price', icon: ICONS.icon_diamond , icon_active : ICONS.icon_diamond_active},
+      ];
+
+
     return(
         <div className="navigation">
             <div className="head-navigation">
@@ -14,44 +33,19 @@ const Navigation = () => {
                 <h5>
                     MAIN MENU
                 </h5>
-                <ul>
-                    <li>
+                <ul className="navigation-bar">
+                    {navigationItems.map((item) => (
+                        <li
+                        key={item.name}
+                        className={activeItem === item.name ? 'isActive' : ''}
+                        onClick={() => handleClick(item.name)}
+                        >
                         <div>
-
+                            <img src={activeItem === item.name ? item.icon_active : item.icon} width="28px" height="28px" alt="" />
                         </div>
-                        Dashboard
-                    </li>
-                    <li>    
-                        <div>
-                            
-                        </div>
-                        Product
-                    </li>
-                    <li>
-                        <div></div>
-                        Order
-                    </li>
-                    <li>
-                        <div></div>
-                        Account
-                    </li>
-                    <li>
-                        <div></div>
-                        Warranty
-                    </li>
-                    <li>
-                        <div></div>
-                        Promotions
-                    </li>
-                    <li>
-                        <div></div>
-                        Gold Price
-                    </li>
-                    <li>
-                        <div></div>
-                        Diamond Price
-                    </li>
-
+                        {item.name}
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
--- admin/src/constants/icons.js
+++ admin/src/constants/icons.js
@@ -1,9 +1,43 @@
 import icon_logo from '../assets/icons/logo.svg';
 import icon_logo_no_jewelry_text from '../assets/icons/logo-nojewelry.svg';
+import icon_dashboard from '../assets/icons/dashboard.svg';
+import icon_diamond from '../assets/icons/diamond.svg'
+import icon_orders from '../assets/icons/orders.svg';
+import icon_user from '../assets/icons/user.svg' 
+import icon_warranty from '../assets/icons/warranty.svg'
+import icon_gold_price from '../assets/icons/gold-price.svg'
+import icon_product from '../assets/icons/product.svg'
+import icon_promotions from '../assets/icons/promotion.svg'
+import icon_dashboard_active from '../assets/icons/dashboard-active.svg'
+import icon_diamond_active from '../assets/icons/diamond-active.svg'
+import icon_gold_price_active from '../assets/icons/gold-price-active.svg'
+import icon_order_active from '../assets/icons/orders-active.svg'
+import icon_product_active from '../assets/icons/product-active.svg'
+import icon_promotion_active from '../assets/icons/promotion-active.svg'
+import icon_user_active from '../assets/icons/user-active.svg'
+import icon_warranty_active from '../assets/icons/warranty-active.svg'
+
+
 
 const ICONS = {
     icon_logo : icon_logo,
-    icon_logo_no_jewelry_text : icon_logo_no_jewelry_text
+    icon_logo_no_jewelry_text : icon_logo_no_jewelry_text,
+    icon_dashboard : icon_dashboard,
+    icon_diamond : icon_diamond,
+    icon_orders : icon_orders,
+    icon_user : icon_user,
+    icon_warranty: icon_warranty,
+    icon_gold_price: icon_gold_price,
+    icon_product : icon_product,
+    icon_promotions :icon_promotions,
+    icon_dashboard_active: icon_dashboard_active,
+    icon_diamond_active: icon_diamond_active,
+    icon_gold_price_active:icon_gold_price_active,
+    icon_order_active:icon_order_active,
+    icon_product_active:icon_product_active,
+    icon_promotion_active:icon_promotion_active,
+    icon_user_active: icon_user_active,
+    icon_warranty_active:icon_warranty_active
 }
 
 export default ICONS;
