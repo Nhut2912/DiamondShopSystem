5c25f8cc69b7e5f7926bb59dc6ee5ab9dfa7f9c0
ThangSE172839 <thangntse172839@fpt.edu.vn>
2024-05-25 07:49:15 +0700

    adjust

--- server/src/main/java/com/example/server/Controller/AccountController.java
+++ server/src/main/java/com/example/server/Controller/AccountController.java
@@ -1,5 +1,11 @@
 package com.example.server.Controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/login")
 public class AccountController {
 
 }
