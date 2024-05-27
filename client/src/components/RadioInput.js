322bd6a9fe99051948d24e92dc36a95cd4a3f3e8
Nhut2912 <nhuttmse172865@fpt.edu.vn>
2024-05-27 22:52:35 +0700

    detail product done:

---
+++ client/src/components/RadioInput.js
@@ -0,0 +1,32 @@
+import React, { useCallback, useState } from 'react'
+
+import '../theme/RadioInput.css';
+
+function RadioInput({ _width, items, setParams, getParams}) {
+  const [active,setActive] = useState(getParams !== undefined && getParams !== null ? getParams : items[0].value) 
+
+  const handleClick = (item) => {
+      
+      setActive(item);
+  }
+
+  return (
+    <div className='radio-input-container' >
+        <div className='container-option-radio'>
+            {items && items.map((item) => (
+              <div
+                style={{"width" : _width }}
+                key={item.value} 
+                className={active === item.value ? 'isActive' : ''}
+                onClick={() => handleClick(item.value)}
+              
+              > 
+                <span>{item.value}</span>
+              </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default RadioInput
