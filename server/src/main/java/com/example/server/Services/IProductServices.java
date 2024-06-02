bb455f973b6b5e7aa5d124fcf7a7a2e9c3d1c835
Gatsby204 <hieuptse172845@fpt.edu.vn>
2024-06-02 18:55:03 +0700

    Update Service

--- server/src/main/java/com/example/server/Services/IProductServices.java
+++ server/src/main/java/com/example/server/Services/IProductServices.java
@@ -1,38 +1,12 @@
 package com.example.server.Services;
 
-<<<<<<< HEAD
-import com.example.server.Pojo.*;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
-
-import java.util.List;
-
-/*
- * author : TRAN MINH NHUT
- * date : 20/5/2024
- * purpose : MO TA CHUC NANG CO TRONG PRODUCTS
- *
- */
-=======
->>>>>>> 308af14b9d231cd737fe4c3dd64cb555c9abde74
-
 import com.example.server.Pojo.Product;
 
 public interface IProductServices {
-<<<<<<< HEAD
-    /*
-     * Author: Pham Trong Hieu
-     * Date: 24/5/2024
-     */
 
-    public ResponseEntity<?> saveProduct(Product product);
-
-=======
 
     public boolean save(Product product);
->>>>>>> 308af14b9d231cd737fe4c3dd64cb555c9abde74
+
 
     public boolean delete(Long ProductID);
 
--- server/src/main/java/com/example/server/Services/ProductServices.java
+++ server/src/main/java/com/example/server/Services/ProductServices.java
@@ -12,23 +12,7 @@ import org.springframework.stereotype.Service;
 public class ProductServices implements IProductServices{
 
     @Autowired
-<<<<<<< HEAD
-    private IProductRepository ProductRepository;
-
-
-    /*
-     * Author: Pham Trong Hieu
-     * Date: 24/5/2024
-     */
-
-    public ResponseEntity<?> saveProduct(Product product) {
-        Product productAfterAdd = ProductRepository.save(product);
-        return new ResponseEntity<>(productAfterAdd, HttpStatus.OK);
-    }
-
-=======
     private IProductRepository productRepository;
->>>>>>> 308af14b9d231cd737fe4c3dd64cb555c9abde74
 
     @Override
     public boolean save(Product product) {
