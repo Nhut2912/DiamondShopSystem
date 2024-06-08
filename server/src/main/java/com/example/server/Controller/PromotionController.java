d6c4062f1229858ce99937f14c9270f9974642c6
ThangSE172839 <thangntse172839@fpt.edu.vn>
2024-06-08 07:37:09 +0700

    upPromo

--- server/src/main/java/com/example/server/Controller/PromotionController.java
+++ server/src/main/java/com/example/server/Controller/PromotionController.java
@@ -5,19 +5,17 @@ import com.example.server.Pojo.Promotion;
 import com.example.server.Service.Promotion.PromotionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Promotion")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PromotionController {
 
     @Autowired
     private PromotionService promotionService;
 
-    @PostMapping
+    @PostMapping("/CreatePromotion")
     public ResponseEntity<PromotionDTO> createPromotion(@RequestBody PromotionDTO promotionDTO){
         Promotion promotion = promotionService.createPromotion(promotionDTO);
 
