a50d9691209540b764c292ae93317c1c9500130c
PhamNgocTrungNhan <nhanpntse182294@fpt.edu.vn>
2024-06-19 07:44:49 +0700

    Add account and goldPrice

---
+++ admin/src/components/HeadTableGoldPrice.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../theme/admin/HeadTableGoldPrice.css'
+
+function HeadTableGoldPrice(){
+   return(
+      <div className='head-table-gold-price-container'>
+          <ul className='head-table-gold-price-content'>
+             <li>Gold Type</li>
+             <li>Buy Price</li>
+             <li>Sell Price</li>
+          </ul>
+      </div>
+   )
+}
+
+export default HeadTableGoldPrice
--- admin/src/pages/Account.js
+++ admin/src/pages/Account.js
@@ -1,76 +1,69 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react' 
 
 import '../../theme/admin/Account.css'
+import HeadTableCardAccount from '../../components/admin/HeadTableCardAccount';
+import CardAccount from '../../components/admin/CardAccount';
+import AddAccount from '../../components/admin/AddAccount';
 
-const Account = () => {
-  const [users, setUsers] = useState([
-    { id: 1, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-    { id: 2, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-    { id: 3, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-    { id: 4, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-    { id: 5, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-    { id: 6, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-    { id: 7, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-    { id: 8, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-    { id: 9, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-    { id: 10, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-    { id: 11, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-    { id: 12, email: '', name: '', phone:'', Address:'', Role:'', Status:'' },
-  ]);
+function Account(){
+  const [activeNavigations,setActiveNavigations] = useState('View Account');
+  const [isAddAccount,setAddAccount] = useState(false);
 
-  const handleDelete = (id) => {
-    setUsers(users.filter(user => user.id !== id));
-  };
+  const navigationAccount = [
+    {name: 'View Account'},
+    {name: 'Add Account'}
+  ]
 
-  const handleAddUser = () => {
-    const newUser = { id: 'new id', email: '', name: '', phone:'', Address:'', Role:'', Status:''  };
-    setUsers([...users, newUser]);
-  };
+  const handleClick = (item) => {
+    if(item === navigationAccount[0].name){
+        setAddAccount(false)
+    }else{
+        setAddAccount(true)
+    }
+    setActiveNavigations(item);
+  }
 
-  return (
-    <div className="manage-account">
-      <div className="main-content">
-        <header>
-          <h1>Manage Account</h1>
-          <button onClick={handleAddUser} className="add-user-btn">+ Add User</button>
-        </header>
-        <table className="users-table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Email</th>
-              <th>Name</th>
-              <th>Phone</th>
-              <th>Address</th>
-              <th>Role</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map(user => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.email}</td>
-                <td>{user.name}</td>
-                <td>{user.phone}</td>
-                <td>{user.address}</td>
-                <td>{user.role}</td>
-                <td>{user.status}</td>
-                <td><button onClick={() => handleDelete(user.id)} className="delete-btn">Delete</button></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="pagination">
-          <button>Previous</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>Next</button>
+  return(
+    <div className='account-container'>
+        <h1>Account</h1>
+        <p>
+          Admin/ <span>Account</span>
+        </p>
+        <div className='account-navigation'>
+            <ul>
+              {
+                navigationAccount.map((item) => (
+                  <li 
+                    key={item.name}
+                    className={item.name === activeNavigations ? 'isActive' : ''}
+                    onClick={() => handleClick(item.name)}
+                  >
+                      {item.name}
+                  </li>
+                ))
+              }
+            </ul>
         </div>
-      </div>
+        <div className='account-content'>
+            {!isAddAccount ? 
+            (
+              <div className='account-view'>
+                  <HeadTableCardAccount />
+                  <div className='account-items-container'>
+                       <CardAccount/>
+                       <CardAccount/>
+                       <CardAccount/>
+                       <CardAccount/>
+                       <CardAccount/>
+                       <CardAccount/>
+                       <CardAccount/>
+                  </div>
+              </div>
+            ) :<AddAccount/>
+            }
+        </div>
     </div>
-  );
-};
+  )
+}
 
 export default Account
