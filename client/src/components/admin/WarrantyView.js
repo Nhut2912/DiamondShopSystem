cc6530328d6d284ab3376e5e4cc5148107a848d0
nhuttmse172865@fpt.edu.vn <nhuttmse172865@fpt.edu.vn>
2024-06-20 01:42:24 +0700

    add warranty

---
+++ client/src/components/admin/WarrantyView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WarrantyView() {
+  return (
+    <div>WarrantyView</div>
+  )
+}
+
+export default WarrantyView
--- client/src/pages/admin/Warranty.js
+++ client/src/pages/admin/Warranty.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import '../../theme/admin/Warranty.css'
+import WarrantyView from '../../components/admin/WarrantyView';
+import WarrantyPolicy from '../../components/admin/WarrantyPolicy';
+
 
 function Warranty() {
+  
+  const navigate = [
+    {name : "View Warranties"},
+    {name : "Warranty Policy"}
+  ]
+
+  const [navigateActive,setNavigateActive] = useState("View Warranties");
+
+
+
+
+  const handleClickNavigate = (value) => {
+    setNavigateActive(value);
+  }
+
   return (
-    <div>Warranty</div>
+    <div className='warranty-container'>
+        <h1>Warranty</h1>
+        <p>
+          Admin / <span>Warranty</span>
+        </p>
+        <div className='warranty-navigation'>
+            <ul className='navigator'>
+                {
+                  navigate.map((item) => (
+                    <li
+                      className={navigateActive === item.name ? "isActive" : null}
+                      onClick={() => handleClickNavigate(item.name)}
+                    >{item.name}</li>
+                  ))
+                }
+            </ul>
+            
+
+            <div className='warranty-content-container'>
+              {
+                false ? <WarrantyView />  : <WarrantyPolicy />
+              }
+                
+            </div>
+
+        </div>
+    </div>
   )
 }
 
