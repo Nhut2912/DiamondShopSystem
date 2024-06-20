fda36904fc50a3d32982a8c305bbf13a926b5380
nhuttmse172865@fpt.edu.vn <nhuttmse172865@fpt.edu.vn>
2024-06-20 10:36:54 +0700

    update warranty policy

--- client/src/components/admin/WarrantyPolicyAdd.js
+++ client/src/components/admin/WarrantyPolicyAdd.js
@@ -1,21 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import "../../theme/admin/WarrantyPolicyAdd.css"
 
 
 function WarrantyPolicyAdd() {
+ 
+ const [name,setName] = useState();
+ const [period,setPeriod] = useState();
+ 
+ const handleChangeName = (event) => {
+    const value = event.target.value;
+    setName(value);
+ }
+
+ const handleChangePeriod = (event) => {
+    const value = event.target.value;
+    setPeriod(value);
+ }
+
+
+ const handleAddWarrantyPolicy = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const warrantyPolicy = {
+        "name": name.trim(),
+        "warrantyPeriod": period,
+        "active": true
+    }
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify(warrantyPolicy),
+        redirect: "follow"
+      };
+      
+      fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/warrantypolicy/create`, requestOptions)
+        .then((response) => response.text())
+        .then((result) => {
+            if(result === "true"){
+                window.location.href = window.location.href
+            }
+        })
+        .catch((error) => console.error(error));
+
+ }
+
   return (
     <div className='warranty-policy-add-container'>
         <h4>Add Warranty Policy</h4>
         <div>
             <label>Name</label>
-            <input type='text' />
+            <input 
+                onChange={handleChangeName}
+            type='text' />
         </div>
         <div>
             <label>Period {"(Month)"}</label>
-            <input type='text' />
+            <input 
+                 onChange={handleChangePeriod}
+            type='text' />
         </div>
-        <div>
+        <div onClick={handleAddWarrantyPolicy}>
             Add Policy
         </div>
     </div>
--- client/src/components/admin/WarrantyPolicyViews.js
+++ client/src/components/admin/WarrantyPolicyViews.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import "../../theme/admin/WarrantyPolicyViews.css"
 
 function WarrantyPolicyViews() {
+ 
+
+ const [data,setData] = useState();
+
+ useEffect(() => {
+    fetch("http://localhost:8080/api/warrantypolicy/get")
+    .then((response) => response.json())
+    .then((result) => {
+        if(result !== null ){
+            setData(result);
+        }
+    })
+    .catch((error) => console.error(error));
+ },[])
+
   return (
     <div className='warranty-policy-views-container'>
        <ul>
@@ -10,16 +25,30 @@ function WarrantyPolicyViews() {
             <li>ID</li>
             <li>Name</li>
             <li>Period {"(Month)"}</li>
-            <li>Active</li>
+            <li>
+                Active
+            </li>
        </ul>
-       <ul>
-            <li>No.</li>
-            <li>ID</li>
-            <li>Name</li>
-            <li>Period {"(Month)"}</li>
-            <li>Active</li>
-       </ul>
-       <div className='line'></div>
+      <div className='container-warranty-policy-card'>
+      {
+        data !== undefined && data !== null && data.length > 0 &&
+        data.map((item,index) => (
+            <>
+                 <ul>
+                        <li>{index+1}</li>
+                        <li>#{item.id}</li>
+                        <li>{item.name}</li>
+                        <li>{item.warrantyPeriod}</li>
+                        <li
+                            className='isActive'
+                        >Active</li>
+                </ul>
+                <div className='line'></div>
+            </>
+        ))
+       }
+      </div>
+      
     </div>
   )
 }
