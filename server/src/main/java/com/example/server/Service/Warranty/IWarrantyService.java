7f600d94e5e9efff9dc9e84e0caa645f922e9763
hiber_neet <hoangtvse172883@fpt.edu.vn>
2024-06-20 17:29:37 +0700

    warranty

--- server/src/main/java/com/example/server/Service/Warranty/IWarrantyService.java
+++ server/src/main/java/com/example/server/Service/Warranty/IWarrantyService.java
@@ -1,7 +1,13 @@
 package com.example.server.Service.Warranty;
 
+import com.example.server.Model.WarrantyDTO;
 import com.example.server.Pojo.Warranty;
 
+import java.util.List;
+
 public interface IWarrantyService {
-    public boolean createWarranty(Warranty warranty);
+    public boolean createWarranty(WarrantyDTO warrantyDTO);
+    public boolean updateWarranty(WarrantyDTO warrantyDTO);
+    public boolean deleteWarranty(WarrantyDTO warrantyDTO);
+    public List<WarrantyDTO> getWarranties();
 }
